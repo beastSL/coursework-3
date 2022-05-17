611acddfb6a2eaf62ed1e9277a3b809c1fbd3272
Sergei Loptev <thatishero@gmail.com>
2022-05-17 21:30:56 +0300

    Add code for model application

--- tests/example.py
+++ tests/example.py
@@ -3,7 +3,7 @@ import numpy as np
 from pprint import pprint
 from sktime.forecasting.model_selection import temporal_train_test_split
 from models.dynamic_regression import DynamicRegression
-from models.models import SktimeETS, SktimeARIMA, SktimeLinearRegression
+from models.models import SktimeETS, SktimeARIMA, SktimeProphet, SktimeNaive
 from sklearn.metrics import mean_squared_error
 import plotly.graph_objects as go
 import warnings
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     print("Testing Dynamic Regression")
     model = DynamicRegression(fh=np.arange(1, 6))
-    status, history = model.fit(train, trace=True, display=True)
+    status, history = model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))), trace=True, display=True)
     print(f"Status {status}, history:{history}")
     pprint(history)
     dynamic_regression_predicts = model.predict(train)
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     print("Testing seasonal Dynamic Regression")
     model = DynamicRegression(fh=np.arange(1, 6), sp=12)
-    status, history = model.fit(train, trace=True, display=True)
+    status, history = model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))), trace=True, display=True)
     print(f"Status {status}, history:")
     pprint(history)
     seasonal_dynamic_regression_predicts = model.predict(train)
@@ -34,21 +34,27 @@ if __name__ == '__main__':
 
     print("Testing ETS")
     model = SktimeETS(auto=True, n_jobs=-1)
-    model.fit(train)
+    model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))))
     ets_predicts = model.predict(fh=np.arange(1, 6))
     print(f"MSE: {mean_squared_error(ets_predicts, test)}")
 
     print("Testing ARIMA")
     model = SktimeARIMA(suppress_warnings=True)
-    model.fit(train)
+    model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))))
     arima_predicts = model.predict(fh=np.arange(1, 6))
     print(f"MSE: {mean_squared_error(arima_predicts, test)}")
 
-    print("Testing Linear Regression")
-    model = SktimeLinearRegression()
-    model.fit(train)
-    linear_regression_predicts = model.predict(fh=np.arange(1, 6))
-    print(f"MSE: {mean_squared_error(linear_regression_predicts, test)}")
+    print("Testing Prophet")
+    model = SktimeProphet()
+    model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))))
+    prophet_predicts = model.predict(fh=np.arange(1, 6)).to_numpy()
+    print(f"MSE: {mean_squared_error(prophet_predicts, test)}")
+
+    print("Testing Naive")
+    model = SktimeNaive()
+    model.fit(pd.Series(train, index=pd.DatetimeIndex(pd.date_range(end='2022-05-01 00:00', freq='M', periods=len(train)))))
+    naive_predicts = model.predict(fh=np.arange(1, 6)).to_numpy()
+    print(f"MSE: {mean_squared_error(naive_predicts, test)}")
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=np.arange(len(train) + len(test)), y=np.append(train, test), name="Test"))
@@ -65,6 +71,9 @@ if __name__ == '__main__':
                              y=np.append([train[len(train) - 1]], arima_predicts),
                              name="ARIMA"))
     fig.add_trace(go.Scatter(x = np.arange(len(train) - 1, len(train) + len(test)),
-                             y=np.append([train[len(train) - 1]], linear_regression_predicts),
-                             name="Linear regression"))
+                             y=np.append([train[len(train) - 1]], prophet_predicts),
+                             name="Prophet"))
+    fig.add_trace(go.Scatter(x = np.arange(len(train) - 1, len(train) + len(test)),
+                             y=np.append([train[len(train) - 1]], naive_predicts),
+                             name="Naive"))
     fig.show()
