fee18ed48a08dbd3bdc9cea1086e97791f93aa21
Sergei Loptev <thatishero@gmail.com>
2022-05-19 15:35:13 +0300

    Fix bug with model application

--- models/models.py
+++ models/models.py
@@ -1,33 +1,4 @@
-from sktime.forecasting.ets import AutoETS
-from sktime.forecasting.arima import AutoARIMA
-from sktime.forecasting.fbprophet import Prophet
-from sktime.forecasting.naive import NaiveForecaster
-
-
-class SktimeETS(AutoETS):
-    '''
-    AutoETS model from sktime.
-    '''
-    def __init__(self, *args, **kwargs):
-        super(SktimeETS, self).__init__(*args, **kwargs)
-
-class SktimeARIMA(AutoARIMA):
-    '''
-    AutoARIMA model from sktime.
-    '''
-    def __init__(self, *args, **kwargs):
-        super(SktimeARIMA, self).__init__(*args, **kwargs)
-
-class SktimeProphet(Prophet):
-    '''
-    PROPHET model from sktime.
-    '''
-    def __init__(self, *args, **kwargs):
-        super(SktimeProphet, self).__init__(*args, **kwargs)
-
-class SktimeNaive(NaiveForecaster):
-    '''
-    Naive model from sktime.
-    '''
-    def __init__(self, *args, **kwargs):
-        super(SktimeNaive, self).__init__(*args, **kwargs)
+from sktime.forecasting.ets import AutoETS as SktimeETS
+from sktime.forecasting.arima import AutoARIMA as SktimeARIMA
+from sktime.forecasting.fbprophet import Prophet as SktimeProphet
+from sktime.forecasting.naive import NaiveForecaster as SktimeNaive
